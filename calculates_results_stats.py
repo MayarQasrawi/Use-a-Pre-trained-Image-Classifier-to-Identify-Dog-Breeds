d2cdfc5cea05fae15b393917a61ce8bcda57a8e2
MayarQasrawi <153677880+MayarQasrawi@users.noreply.github.com>
2024-05-30 21:02:28 +0300

    Add files via upload

--- calculates_results_stats.py
+++ calculates_results_stats.py
@@ -97,12 +97,13 @@ def calculates_results_stats(results_dic):
     results_stats_dic['n_notdogs_img'] = (results_stats_dic['n_images'] - 
                                       results_stats_dic['n_dogs_img']) 
 
-    results_stats_dic['pct_match'] = (results_stats_dic["n_match"] / results_stats_dic["n_images"]) * 100
+    results_stats_dic['pct_match'] = (results_stats_dic["n_match"] / results_stats_dic["n_images"]) * 100.0
 
-    results_stats_dic['pct_correct_dogs'] = (results_stats_dic["n_correct_dogs"] / results_stats_dic["n_correct_dogs"]) * 100
+    results_stats_dic['pct_correct_dogs'] = (results_stats_dic["n_correct_dogs"] / results_stats_dic["n_dogs_img"]) * 100.0
 
-    results_stats_dic['pct_correct_breed'] = (results_stats_dic["n_correct_breed"] / results_stats_dic["n_correct_breed"]) * 100
-
+    
+    results_stats_dic['pct_correct_breed'] = results_stats_dic['n_correct_breed'] / results_stats_dic['n_dogs_img'] * 100.0
+    
     if results_stats_dic['n_notdogs_img'] > 0:
         results_stats_dic['pct_correct_notdogs'] = (results_stats_dic['n_correct_notdogs'] /
                                                 results_stats_dic['n_notdogs_img'])*100.0
